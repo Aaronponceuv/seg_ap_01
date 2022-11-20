8006c0182e8b0abe8ccfca93f4c00149c8599224
AaronPonceSandoval <aaron.ponce@alumnos.uv.cl>
2022-11-20 14:20:24 -0300

    agrega arreglos de dice metrics

--- test.py
+++ test.py
@@ -11,9 +11,14 @@ from keras import utils
 import numpy as np
 import json
 import nrrd
-
+import keras
 from metricas import dice_arteria_derecha
 from matplotlib import pyplot as plt
+from matplotlib.colors import Colormap, ListedColormap
+
+import wandb
+from monai.transforms import Resized
+
 
 def fix_gpu():
     config = ConfigProto()
@@ -38,6 +43,30 @@ def rediminesionar(volumen):
 
     return data_new
 
+def recorrer(segmentacion,map=None):
+    segmentacion = np.asarray(segmentacion)
+    segmentacion = segmentacion[~(segmentacion==0).all((2,1))]
+    seg = segmentacion
+    import math
+    columna = 0
+    fila = 0
+    fig, ax = plt.subplots(4,int(math.ceil(seg.shape[0] / 4)),figsize=(20,10))
+    map=ListedColormap(["indigo", "yellow", "green", "green"])
+    for indice in range(0,seg.shape[0]):
+ 
+        ax[fila,columna].imshow(seg[indice,:,:],cmap=map)
+        ax[fila,columna].set_title("fase {}".format(str(indice)))
+        #ax[fila,columna].axis(False)
+            
+        if(columna == int(math.ceil(seg.shape[0] / 4)) -1):
+            columna = 0
+            fila+=1
+        else:
+            columna +=1
+        if(fila==5 and columna == 4):
+            break
+    plt.show()
+
 
 def test_modelo(ruta_modelo,conjuntos, imagenes, etiquetas):
 
@@ -49,6 +78,9 @@ def test_modelo(ruta_modelo,conjuntos, imagenes, etiquetas):
     detecciones = []
     true_etiqueta = []
     inputs = []
+    true_etiquetas_no_binarias = []
+    #redim_m = Resized(keys=["image", "seg","seg_art_principal","seg_art_derecha","seg_art_izquierda"], spatial_size=(128,128,64), mode=('nearest'))
+    #data_dict = redim_m(data_dict)
     for id in conjuntos:
         ruta_imagen = imagenes[id] 
         ruta_etiqueta = etiquetas[id]
@@ -61,16 +93,33 @@ def test_modelo(ruta_modelo,conjuntos, imagenes, etiquetas):
         imagen = np.expand_dims(imagen, axis=0)
 
 
-        etiqueta, _ = nrrd.read(ruta_etiqueta)
+        etiqueta_no_binaria, _ = nrrd.read(ruta_etiqueta)
+        true_etiquetas_no_binarias.append(etiqueta_no_binaria)
 
-        plt.imshow(etiqueta[:,:,32])
+        arteria_principal = etiqueta_no_binaria == 1
+        arteria_derecha = etiqueta_no_binaria == 2
+        arteria_izquierda = etiqueta_no_binaria == 3
+
+
+        arteria_principal = np.expand_dims(arteria_principal, axis=0)
+        arteria_derecha = np.expand_dims(arteria_derecha, axis=0)
+        arteria_izquierda = np.expand_dims(arteria_izquierda, axis=0)
+
+        #Resized(arteria_izquierda,mode="nearest")
+
+
+        plt.imshow(etiqueta_no_binaria[:,:,32])
         plt.savefig("test0.png")
 
-        etiqueta = utils.to_categorical(etiqueta, num_classes=4)
+        etiqueta = utils.to_categorical(etiqueta_no_binaria, num_classes=4)
         etiqueta = rediminesionar(etiqueta)
+
+        print("etiqueta redim : ",etiqueta.shape)
 
         plt.imshow(etiqueta[:,:,32])
         plt.savefig("test1.png")
+
+        
 
         etiqueta = (etiqueta > 0).astype(int)
 
@@ -96,7 +145,9 @@ def test_modelo(ruta_modelo,conjuntos, imagenes, etiquetas):
         true_etiqueta.append(np.array(etiqueta[0]).reshape(128,128,64,4))
 
     print("true_etiqueta: {}".format(true_etiqueta[0].shape))
-    print(np.unique(detecciones[0][:,:,32,2]))
+    print(np.max(detecciones[0][:,:,32,2]))
+
+
 
     fig, ax = plt.subplots(5,7, figsize=(10,10))
     ax[0,0].imshow(inputs[0][:,:,32], cmap="gray")
@@ -130,19 +181,109 @@ def test_modelo(ruta_modelo,conjuntos, imagenes, etiquetas):
     ax[3,4].imshow(detecciones[3][:,:,32,2]>0.7, cmap="gray")
     ax[3,5].imshow(detecciones[3][:,:,32,2]>0.9, cmap="gray")
     ax[3,6].imshow(true_etiqueta[3][:,:,32,1])
-
+                                                                                                                                                                                 
     ax[4,0].imshow(inputs[4][:,:,32], cmap="gray")
-    ax[4,1].imshow(detecciones[4][:,:,32,2]>0.1, cmap="gray")
-    ax[4,2].imshow(detecciones[4][:,:,32,2]>0.3, cmap="gray")
-    ax[4,3].imshow(detecciones[4][:,:,32,2]>0.5, cmap="gray")
-    ax[4,4].imshow(detecciones[4][:,:,32,2]>0.7, cmap="gray")
-    ax[4,5].imshow(detecciones[4][:,:,32,2]>0.9, cmap="gray")
+    ax[4,1].imshow((detecciones[4][:,:,32,2]>0.1).astype(np.int8), cmap="gray")
+    ax[4,2].imshow((detecciones[4][:,:,32,2]>0.3).astype(np.int8), cmap="gray")
+    ax[4,3].imshow((detecciones[4][:,:,32,2]>0.5).astype(np.int8), cmap="gray")
+    ax[4,4].imshow((detecciones[4][:,:,32,2]>0.7).astype(np.int8), cmap="gray")
+    ax[4,5].imshow((detecciones[4][:,:,32,2]>0.9).astype(np.int8), cmap="gray")
     ax[4,6].imshow(true_etiqueta[4][:,:,32,1])
 
     plt.savefig("test2.png")
 
+    print("nop unique: ",np.unique((detecciones[4][:,:,32,2]>0.5).astype(np.int8)))
+    print("detecciones[0][:,:,:,:]: ",detecciones[0][:,:,:,:].shape)
+
+    print("np unique etiqueta ", np.unique(true_etiquetas_no_binarias[0][:,32,:]))
+
+    plt.clf()
+    plt.close()
+
+    plt.imshow(true_etiquetas_no_binarias[0][:,32,:])
+    plt.savefig("test_true.png")
+
+    mask_data = np.array([[1, 2, 2, ... , 2, 2, 1], ...])
+
+    class_labels = {
+        0: "Background",
+        1: "Art. pulmonar principal",
+        2: "Art. pulmonar derecha",
+        3: "Art. pulmonar izquierda"
+    }
+
+
+
+    img = (inputs[0][:,:,32]).astype(np.float16)
+
+
+    mask_img = wandb.Image(img, masks={
+                "predictions": {
+                    "mask_data": detecciones[0][:,:,32,2],
+                    "class_labels": class_labels
+                },
+                "ground_truth" : {
+                        "mask_data" : true_etiquetas_no_binarias[0][:,32,:],
+                        "class_labels" : class_labels
+                    }
+
+            })
+
+    wandb.log({"test":mask_img})
+
+
+    img = (inputs[0][:,:,32]).astype(np.int8) * 255
+    mask_img = wandb.Image(img, masks={
+                "predictions": {
+                    "mask_data": detecciones[0][:,32,2],
+                    "class_labels": class_labels
+                },
+                "ground_truth" : {
+                        "mask_data" : true_etiquetas_no_binarias[0][:,32,:],
+                        "class_labels" : class_labels
+                    }
+
+            })
+
+    wandb.log({"test":mask_img})
+
+    img = (inputs[0][:,:,32]).astype(np.int8) * 255
+    mask_img = wandb.Image(img, masks={
+                "predictions": {
+                    "mask_data": detecciones[0][:,32,2],
+                    "class_labels": class_labels
+                },
+                "ground_truth" : {
+                        "mask_data" : (true_etiquetas_no_binarias[0][:,32,:]),
+                        "class_labels" : class_labels
+                    }
+
+            })
+
+    wandb.log({"test":mask_img})
+
+    img = (inputs[0][:,:,32]).astype(np.int8) * 255
+    mask_img = wandb.Image(img, masks={
+                "predictions": {
+                    "mask_data": detecciones[0][:,:,32,2],
+                    "class_labels": class_labels
+                },
+                "ground_truth" : {
+                        "mask_data" : true_etiquetas_no_binarias[0][:,32,:],
+                        "class_labels" : class_labels
+                    }
+
+            })
+
+    wandb.log({"test":mask_img})
+
+
+    #recorrer(detecciones[4][:,:,32,2]>0.3)
+
 
 if __name__ == "__main__":
+
+    wandb.init(settings=wandb.Settings(start_method="fork"))
 
     dim=(128, 128, 64,1)
 
